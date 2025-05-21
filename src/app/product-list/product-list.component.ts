a63ba4dcb5a77f98b0ff2b9e1bef9302e4b00a48
talipottech <talipottech@hotmail.com>
2025-05-21 08:31:23 +0530

    component interaction
    passing data from one component to another component

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Product } from '../product';
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [ProductDetailComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
@@ -12,4 +13,11 @@ products: Product[] = [{ id: 1, title: 'Keyboard' },
     { id: 2, title: 'Microphone' },
     { id: 3, title: 'Web camera' },
     { id: 4, title: 'Tablet' }];
+
+    selectedProduct: Product | undefined;
+    onAdded() {
+  alert(`${this.selectedProduct?.title} added to the cart!`);
 }
+
+}
+
